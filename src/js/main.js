0373d4824dc14b78ac664a6b4288179a0c8a055d
alfari16 <alif.anshory@yahoo.com>
2017-09-27 19:24:46 +0700

    fixed nav + footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-
+function navigasi(wrapper, wrapperName, btn) {
+  var menuTrigger=$(wrapper);
+  $(btn).click(function() {
+    menuTrigger.addClass('active');
+  });
+  menuTrigger.click(function(e) {
+    if($(e.target).hasClass(wrapperName)){
+      menuTrigger.removeClass('active');
+    }
+  });
+}
+$(document).ready(function() {
+  navigasi('.md-wrapper','md-wrapper','.btn-dropdown');
+  navigasi('.nav-collapse-xs','nav-collapse-xs','.btn-nav');
+  $('.close-btn').click(function() {
+    $('.nav-collapse-xs').removeClass('active');
+  });
+});
